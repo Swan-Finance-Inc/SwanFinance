a3ee5009eaa18c5ab74fbb7601c66d937c63467e
Zaryab Afser <zaryab.a@quillhash.com>
2020-12-09 18:31:48 +0530

    updated Crowdsale Contract

--- contracts/Crowdsale.sol
+++ contracts/Crowdsale.sol
@@ -165,7 +165,7 @@ contract Crowdsale is Pausable {
 
   Swan public token;
 
-  uint256 public hardCap = 0; 
+  uint256 public hardCap; 
   uint256 public constant softCap = 500000000; // in cents 
 
   //total tokens for sale 
@@ -173,12 +173,12 @@ contract Crowdsale is Pausable {
   uint256 public tokensForSale = 140000000000 ether;
 
   // tokens sold in each round of sale 
-  uint256 public privateSaletokenSold = 0;
-  uint256 public preSaleTokensSold = 0;
-  uint256 public crowdSaleRoundOne = 0;
-  uint256 public crowdSaleRoundTwo = 0;
-  uint256 public crowdSaleRoundThree = 0;
-  uint256 public crowdSaleRoundFour = 0;
+  uint256 public privateSaletokenSold;
+  uint256 public preSaleTokensSold;
+  uint256 public crowdSaleRoundOne;
+  uint256 public crowdSaleRoundTwo;
+  uint256 public crowdSaleRoundThree;
+  uint256 public crowdSaleRoundFour;
 
   // tokenSoldLimit in rounds
   uint256 public constant privateSaletokenLimit = 2500000000;
@@ -196,14 +196,14 @@ contract Crowdsale is Pausable {
   uint256 public constant bonusPercentRoudOne = 15;
   uint256 public constant bonusPercentRoudTwo = 10;
   uint256 public constant bonusPercentRoudThree = 5;
-  uint256 public constant bonusPercentRoudFour = 0;  
+  uint256 public constant bonusPercentRoudFour;  
 
   // user limit
   uint256 public constant minimumInvestment = 50000;
   uint256 public constant maximumInvestment = 20000000;
   uint256 public constant tokensInOneDollar = 1000;
 
-  bool public crowdSaleStarted = false;
+  bool public crowdSaleStarted;
 
   enum Stages {CrowdSaleNotStarted, Pause, PrivateSaleStart,PrivateSaleEnd, PreSaleStart, PreSaleEnd, CrowdSaleRoundOneStart,CrowdSaleRoundOneEnd, CrowdSaleRoundTwoStart, CrowdSaleRoundTwoEnd, CrowdSaleRoundThreeStart, CrowdSaleRoundThreeEnd, CrowdSaleRoundFourStart, CrowdSaleRoundFourEnd}
 
@@ -253,14 +253,17 @@ contract Crowdsale is Pausable {
         buyTokens(msg.sender); 
      }
      else{
-     revert("This is the error");
+     revert("Caller is Owner Itself");
      }
      
     }
+
+    //@notice returns the total Swan Tokens balance of Crowdsale Contract
     function getSwanTokenBalance() public view returns(uint256){
       return Swan(token).balanceOf(address(this));
     }
 
+    //@notice returns the total Wei Amount raised till date
     function getTotalWeiRaised() external view returns(uint256){
       return totalWeiRaised;
     }
@@ -285,7 +288,7 @@ contract Crowdsale is Pausable {
     
     function pause() external onlyOwner {
       require(!Paused,"contract is already Paused");
-      require(crowdSaleStarted,"Crowdsale already started");
+      require(crowdSaleStarted,"Crowdsale did not start yet");
       previousStage=currentStage;
       currentStage=Stages.Pause;
       Paused = true;
@@ -304,80 +307,58 @@ contract Crowdsale is Pausable {
     }
 
     function endPrivateSale() external onlyOwner {
-
       require(currentStage == Stages.PrivateSaleStart,"Crowdsale didn't Start yet");
       currentStage = Stages.PrivateSaleEnd;
-
     }
 
     function startPreSale() external onlyOwner {
-
     require(currentStage == Stages.PrivateSaleEnd,"Private Sale Didn't end yet");
     currentStage = Stages.PreSaleStart;
-   
     }
 
     function endPreSale() external onlyOwner {
-
     require(currentStage == Stages.PreSaleStart,"Pre Sale didn't Start yet");
     currentStage = Stages.PreSaleEnd;
-   
     }
 
     function startCrowdSaleRoundOne() external onlyOwner {
-
     require(currentStage == Stages.PreSaleEnd,"Pre Sale Didn't end yet");
     currentStage = Stages.CrowdSaleRoundOneStart;
-
     }
 
     function endCrowdSaleRoundOne() external onlyOwner {
-
     require(currentStage == Stages.CrowdSaleRoundOneStart,"CrowdSaleRoundOne Didn't start yet");
     currentStage = Stages.CrowdSaleRoundOneEnd;
-
     }
 
     function startCrowdSaleRoundTwo() external onlyOwner {
-
     require(currentStage == Stages.CrowdSaleRoundOneEnd,"CrowdSaleRoundOne Didn't end yet");
     currentStage = Stages.CrowdSaleRoundTwoStart;
-
     }
 
     function endCrowdSaleRoundTwo() external onlyOwner {
-
     require(currentStage == Stages.CrowdSaleRoundTwoStart,"CrowdSaleRoundTwo Didn't start yet");
     currentStage = Stages.CrowdSaleRoundTwoEnd;
-
     }
 
     function startCrowdSaleRoundThree() external onlyOwner {
-
     require(currentStage == Stages.CrowdSaleRoundTwoEnd,"CrowdSaleRoundTwo Didn't end yet");
     currentStage = Stages.CrowdSaleRoundThreeStart;
-
     }
 
     function endCrowdSaleRoundThree() external onlyOwner {
-
     require(currentStage == Stages.CrowdSaleRoundThreeStart,"CrowdSaleRoundThree Didn't start yet");
     currentStage = Stages.CrowdSaleRoundThreeEnd;
-
     }
 
     function startCrowdSaleRoundFour() external onlyOwner {
-
     require(currentStage == Stages.CrowdSaleRoundThreeEnd,"CrowdSaleRoundThree Didn't end yet");
     currentStage = Stages.CrowdSaleRoundFourStart;
-
     }
 
     function endCrowdSaleRoundFour() external onlyOwner {
-
     require(currentStage == Stages.CrowdSaleRoundFourStart,"CrowdSaleRoundFour Didn't start yet");
     currentStage = Stages.CrowdSaleRoundFourEnd;
-
     }
 
     function getStage() external view returns (string memory) {
@@ -489,7 +470,7 @@ contract Crowdsale is Pausable {
 
    }
    /**
-   * @dev Executed when a purchase has been validated and is ready to be executed. Not necessarily emits/sends tokens.
+   * @dev Executed when a purchase has been validated and is ready to be executed.
    * @param _beneficiary Address receiving the tokens
    * @param _tokenAmount Number of tokens to be purchased
    */
@@ -563,13 +544,13 @@ contract Crowdsale is Pausable {
     /**
     * @dev finalize the crowdsale.After finalizing ,tokens transfer can be done.
     */
-    function finalizeSale() public  onlyOwner {
-        require(currentStage == Stages.CrowdSaleRoundFourEnd,"CrowdSaleRoundFour didn't end yet");
-        uint256 availableTokens = getSwanTokenBalance();
-        if(availableTokens > 0){
-          require(token.transfer(owner, availableTokens),"Transfer Failed");
-        }else{
-          revert("Zero Tokens Left in Contract");
-        }
+    function finalizeSale() external  onlyOwner {
+      require(currentStage == Stages.CrowdSaleRoundFourEnd,"CrowdSaleRoundFour didn't end yet");
+      uint256 availableTokens = getSwanTokenBalance();
+      if(availableTokens > 0){
+        require(token.transfer(owner, availableTokens),"Transfer Failed");
+      }else{
+        revert("Zero Tokens Left in Contract");
+      }
     }
 }
