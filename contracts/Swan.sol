5add01f102c2f98e8c27cf812ee17e0524c3aedc
tpikachu <46133358+tpikachu@users.noreply.github.com>
2020-12-28 18:40:31 -0700

    Gitignore

--- contracts/Swan.sol
+++ contracts/Swan.sol
@@ -4,13 +4,12 @@ import "./Owned.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
 
-
-contract Swan is Owned,ERC20Detailed,ERC20{
-    constructor(address _owner) 
-        ERC20Detailed("Swan Finance","SWAN",18)
-        Owned(_owner)
+contract Swan is Owned, ERC20Detailed, ERC20 {
+    constructor(address _owner)
         public
+        ERC20Detailed("Swan Finance", "SWAN", 18)
+        Owned(_owner)
     {
-        _mint(_owner,50000000000 * (10 ** uint256(decimals())));
+        _mint(_owner, 50000000000 * (10**uint256(decimals())));
     }
 }
